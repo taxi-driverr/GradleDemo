f67930ca61bb0e27cc09098c8d97c6fe35de0cbc
taxi_driverr <xerxesxerxes13@gmail.com>
2026-05-19 18:20:52 +0530

    added okhttp to make http requests

--- READMe.md
+++ READMe.md
@@ -1,15 +1,20 @@
 Setup of basic gradle using cli:
+```
 gradle init --type java-application --dsl groovy
+```
 The above command will setup a basic gradle project
 
+```
 ./gradlew tasks
+```
 The above command act as a helper
-
+```
 ./gradlew build
+```
 The above command can build your project
 
 To prepare a jar which is executable, you need to setup manifest property in build.gradle to identify what is the main class to execute:
-
+```
 vi build.gradle
 i, 
 jar {
@@ -20,9 +25,14 @@ attributes (
 }
 }
 esc, :wq
+```
 
+```
 ./gradlew jar
+```
 The above command creates a new jar file in build/libs folder
 
+```
 java -jar build/libs/filename.jar
+```
 The above command will execute your code
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,33 @@
 package org.example;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        OkHttpClient client = new OkHttpClient();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        String url = "https://jsonplaceholder.typicode.com/todos/1/";
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if(!response.isSuccessful()) {
+                System.out.println("Something went wring !!!!");
+            }
+            System.out.println(response.body().string());
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
+
+
+
     }
 }
